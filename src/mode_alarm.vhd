bdc1ea8d59617f2bc4dcf531b92d33b771a31fbc
smaria <sophia.schillai@mytum.de>
2013-01-18 07:09:21 +0000

    fix most errors

--- src/mode_alarm.vhd
+++ src/mode_alarm.vhd
@@ -1,42 +1,40 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
-library work;
-use work.main_pkg.all;
+use work.main_pkg.all; -- was fuer eine library wird da verwendet??
 
 entity mode_alarm is
   port(
 -- clk, reset
-    uni:            in  universal_signals;
+    uni: in universal_signals;
 -- kc_minus_imp, kc_plus_imp, kc_act_imp
-    key:            in  keypad_signals;
+    key: in keypad_signals;
 -- current_time
-    ctime:          in  time_signals;
-    keyboard_focus: in  std_logic(3 downto 0);
+    ctime: in time_signals;
+    keyboard_focus: in std_logic(3 downto 0);
 -- output, mode_alarm is 1 ??
-    characters:     out character_array_3d(2 downto 0,3 downto 0, 19 downto 0);
-    alarm_active:   out std_logic;
+    characters: out character_array_3d(2 downto 0,3 downto 0, 19 downto 0);
+    alarm_active: out std_logic;
 -- alarm is ringing
-    alarm_on:       out std_logic;
--- alarm LED 
-    al_on:          out std_logic
+    alarm_on: out std_logic;
+-- alarm LED
+    al_on: out std_logic
   );
 end mode_alarm;
 
 architecture behavioral of mode_alarm is
   type alarm_state_t is (
-    ALARM_ON_0
-    ALARM_ON_1
-    SNOOZE
+    ALARM_ONA,
+    ALARM_ONB,
+    SNOOZE,
     ALARM_OFF
   );
-  signal alarm_state:   alarm_state_t;
+  signal alarm_state: alarm_state_t;
 
-  signal snooze_hour:   std_logic_vector(5 downto 0);
+  signal snooze_hour: std_logic_vector(5 downto 0);
   signal snooze_minute: std_logic_vector(6 downto 0);
-  signal alarm_hour:    std_logic_vector(5 downto 0);
-  signal alarm_minute:  std_logic_vector(6 downto 0);
+  signal alarm_hour: std_logic_vector(5 downto 0);
+  signal alarm_minute: std_logic_vector(6 downto 0);
 
 
 begin
@@ -50,24 +48,24 @@ begin
       if uni.reset = '1' then
         alarm_on<='0';
         alarm_hour<="000000";
-        alarm_minute<="000111";   -- 7 minuten
+        alarm_minute<="000111"; -- 7 minuten
 
-      elsif keyboard_focus = "0010" then     -- check keyboard focus
+      elsif keyboard_focus = "0010" then -- check keyboard focus
         if alarm_active='0' then
       -- set Alarm time
           if key.kc_minus_imp = '1' then
-            if alarm_minute="0000000" then       -- Reduce hours
-              if alarm_hour="000000" then        -- go before Midnight
-                alarm_hour<="010111";    -- 23 hrs
+            if alarm_minute="0000000" then -- Reduce hours
+              if alarm_hour="000000" then -- go before Midnight
+                alarm_hour<="010111"; -- 23 hrs
               else
                 alarm_hour<= std_logic_vector( unsigned(alarm_hour) -1);
               end if;
-              alarm_minute<="0111011";      -- 59 minutes
-            else                          -- normal jump of just one minute
+              alarm_minute<="0111011"; -- 59 minutes
+            else -- normal jump of just one minute
               alarm_minute<= std_logic_vector( unsigned(alarm_minute) -1);
             end if;
           elsif key.kc_plus_imp = '1' then
-            if alarm_minute="0111011" then -- 59 minutes 
+            if alarm_minute="0111011" then -- 59 minutes
               if alarm_hour="010111" then -- 23 hours
                 alarm_hour<="000000";
               else
@@ -79,16 +77,16 @@ begin
             end if;
       -- Alarm active/inactive
           elsif key.kc_act_imp = '1' then
-            alarm_active <= -alarm_active;   -- negate current value (??)
+            alarm_active <= -alarm_active; -- negate current value (??)
             if alarm_active = '1' then
-              star='*';
+              star<='*';
             else
-              star=' ';
+              star<=' ';
             end if;
           end if;
       -- print display output
-        characters(1,2,19 downto 0) <= star+"     Alarm:        "     -- display output
-        characters(1,3,19 downto 0) <= "       "+to_integer(alarm_hour)+":"+to_integer(alarm_minute)+"        "
+        characters(1,2,19 downto 0) <= star+" Alarm: "; -- display output
+        characters(1,3,19 downto 0) <= " "+to_integer(alarm_hour)+":"+to_integer(alarm_minute)+" ";
       end if;
     end if;
   end process;
@@ -99,22 +97,22 @@ begin
   process(uni.clk)
   begin
     if rising_edge(uni.clk) then
-      if alarm_active = '1' then      -- check if alarm active
-        if alarm_state=ALARM_OFF then 
-          if ((alarm_hour=ctime.hour)and(alarm_minute=ctime.minute) then
-            alarm_state<=ALARM_ON_0;
+      if alarm_active = '1' then -- check if alarm active
+        if alarm_state=ALARM_OFF then
+          if ((alarm_hour=ctime.hour)and(alarm_minute=ctime.minute)) then
+            alarm_state<=ALARM_ONA;
             alarm_on<='1';
             al_on<='1';
             snooze_hour<=alarm_hour;
             snooze_minute<=alarm_minute;
           end if;
-        elsif (alarm_state=ALARM_ON_0) or (alarm_state=ALARM_ON_1) then
+        elsif (alarm_state=ALARM_ONA) or (alarm_state=ALARM_ONB) then
           if key.kc_act_imp='1' then
             alarm_state<=SNOOZE;
             alarm_on<='0';
             al_on<='0';
-            for i in 0 to 4 loop      -- add 1 minute five times
-              if snooze_minute="0111011" then  -- 59 mins
+            for i in 0 to 4 loop -- add 1 minute five times
+              if snooze_minute="0111011" then -- 59 mins
                 if snooze_hour="010111" then -- 23 hrs
                   snooze_hour<="000000";
                else
@@ -127,16 +125,16 @@ begin
             end loop;
           elsif key.kc_act_long='1' then
             alarm_state<=ALARM_OFF;
-            al_on='0';
-            alarm_on='0';
-          elsif snooze_minute/=ctime.minute then  -- now about one minute should have passed
+            al_on<='0';
+            alarm_on<='0';
+          elsif snooze_minute/=ctime.minute then -- now about one minute should have passed
             alarm_state<=ALARM_OFF;
-            an_on='0';
-            alarm_on='0';
+            an_on<='0';
+            alarm_on<='0';
           end if;
         elsif alarm_state=SNOOZE then
           if (snooze_hour=ctime.hour)and(snooze_minute=ctime.minute) then
-          alarm_state<=ALARM_ON_1;
+          alarm_state<=ALARM_ONB;
           al_on<='1';
           alarm_on<='1';
         end if;
