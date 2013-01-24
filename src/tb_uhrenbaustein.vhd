54f8d931025a560523d612fe7533a51d558e521a
smaria <sophia.schillai@mytum.de>
2013-01-24 09:14:58 +0000

    add basic structures tb_mode_alarm tb_uhrenbaustein

---
+++ src/tb_uhrenbaustein.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.add;
+
+library std;
+use std.textio.all;
+
+library work;
+use work.main_pkg.all;
+
+entity tb_mode_alarm is
+end entity;
+
+
+architecture behavioral of tb_mode_alarm is
+  signal uni        :universal_signals;
+
+
+
+end architecture;
